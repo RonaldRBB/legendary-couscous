a633c95a01699bdc97ccc0ecc7b22530ee62a12e
Ronald Bello <ronaldbello2@gmail.com>
2023-11-05 17:35:34 -0300

    tile mod

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import TileClients from './tile_clients';
-import TileSales from './tile_sales';
-import TilePerformance from './tile_performance';
-import CardPerformance from './card/performance';
-import Clients from './card/clients';
+import CardPerformance from './cards/performance';
+import Clients from './cards/clients';
+import Tile from './tiles/tile';
 class Main extends React.Component {
+    tilesList() {
+        return [
+            {
+                title: "Clients",
+                icon: "mdi mdi-account-multiple",
+                color: "has-text-primary",
+                value: "512"
+            },
+            {
+                title: "Sales",
+                icon: "mdi mdi-cart-outline",
+                color: "has-text-info",
+                value: "$7,770"
+            },
+            {
+                title: "Performance",
+                icon: "mdi mdi-finance",
+                color: "has-text-success",
+                value: "256%"
+            }
+        ]
+    }
     render() {
         return (
             <section className="section is-main-section">
                 <div className="tile is-ancestor">
-                    <TileClients />
-                    <TileSales />
-                    <TilePerformance />
+                    {this.tilesList().map(tile =>
+                        <Tile
+                            title={tile.title}
+                            icon={tile.icon}
+                            color={tile.color}
+                            value={tile.value}
+                        />)}
                 </div>
                 <CardPerformance />
                 <Clients />
--- src/components/tiles/tile.jsx
+++ src/components/tiles/tile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-class TileSales extends React.Component {
+class Tile extends React.Component {
     render() {
         return (
             <div className="tile is-parent">
@@ -8,14 +8,14 @@ class TileSales extends React.Component {
                         <div className="level is-mobile">
                             <div className="level-item">
                                 <div className="is-widget-label">
-                                    <h3 className="subtitle is-spaced">Sales</h3>
-                                    <h1 className="title">$7,770</h1>
+                                    <h3 className="subtitle is-spaced">{this.props.title}</h3>
+                                    <h1 className="title">{this.props.value}</h1>
                                 </div>
                             </div>
                             <div className="level-item has-widget-icon">
                                 <div className="is-widget-icon">
-                                    <span className="icon has-text-info is-large">
-                                        <i className="mdi mdi-cart-outline mdi-48px" />
+                                    <span className={"icon " + this.props.color + " is-large"}>
+                                        <i className={this.props.icon + " mdi-48px"} />
                                     </span>
                                 </div>
                             </div>
@@ -26,4 +26,4 @@ class TileSales extends React.Component {
         );
     }
 }
-export default TileSales
+export default Tile
